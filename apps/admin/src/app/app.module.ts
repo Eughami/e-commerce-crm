5212f5752a8171b7f645ed78c099a2a1ce9bcd34
Eughami <imamosi50@gmail.com>
2022-03-10 23:18:10 +0200

    added typeorm config

--- apps/admin/src/app/app.module.ts
+++ apps/admin/src/app/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'eughami',
+      password: 'password',
+      database: 'eughami',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
