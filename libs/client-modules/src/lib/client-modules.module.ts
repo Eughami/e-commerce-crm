7583d058bcdac7a67598df469bcf878ce6223214
dabeerofficial <dabeer.r@mblhightech.com>
2022-03-20 15:28:22 +0200

    fixed the user auth subscriber/validate-password issue

--- libs/client-modules/src/lib/client-modules.module.ts
+++ libs/client-modules/src/lib/client-modules.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UserAuthModule, UserModule, UserRepository, UserSubscriber } from '@shopping/core-modules';
+import { UserAuthModule, UserModule, UserRepository } from '@shopping/core-modules';
 import { User } from '@shopping/entities';
 
 @Module({
-  imports: [UserAuthModule, UserModule, UserSubscriber, TypeOrmModule.forFeature([UserRepository, User])],
+  imports: [UserAuthModule, UserModule, TypeOrmModule.forFeature([UserRepository, User])],
   controllers: [],
   providers: [],
   exports: []
